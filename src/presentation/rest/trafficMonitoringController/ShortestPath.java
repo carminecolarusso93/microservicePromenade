54855a8e9fa2e76f3d8cbdc27fd4ee550239f680
colarusso <colarusso@colarussoMSI>
2021-03-24 21:39:21 +0100

    Refactoring for Promenade

--- src/presentation/rest/trafficMonitoringController/ShortestPath.java
+++
@@ -1,9 +0,0 @@
-package presentation.rest.trafficMonitoringController;
-
-import data.dataModel.Coordinate;
-
-import java.util.ArrayList;
-
-public class ShortestPath {
-    public ArrayList<Coordinate> shortestPath;
-}
